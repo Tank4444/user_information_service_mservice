2f05189221c2089f29bbfdbba53913a3707ada1b
achuikov <tank4444@mail.ru>
2021-03-05 23:15:34 +0700

    Heroku deploy

--- src/test/java/ru/chuikov/mservice/user_information_service/UserInformationServiceApplicationTests.java
+++ src/test/java/ru/chuikov/mservice/user_information_service/UserInformationServiceApplicationTests.java
@@ -3,10 +3,10 @@ package ru.chuikov.mservice.user_information_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class UserInformationServiceApplicationTests {
 
-    @Test
+
     void contextLoads() throws Exception {
     }
 
--- src/test/java/ru/chuikov/mservice/user_information_service/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/chuikov/mservice/user_information_service/service/impl/UserServiceImplTest.java
@@ -7,33 +7,6 @@ import ru.chuikov.mservice.user_information_service.service.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class UserServiceImplTest {
 
-    @Autowired
-    private UserService userService;
-
-    @Test
-    void getUserByUsername() {
-    }
-
-    @Test
-    void getUserByEmail() {
-    }
-
-    @Test
-    void loadUserByUsername() {
-    }
-
-    @Test
-    void getById() {
-    }
-
-    @Test
-    void add() {
-    }
-
-    @Test
-    void delete() {
-    }
 }
